401086b2af0ada3cdc11c82049902a170a1c9acf
Nihal Williams <nihalwill@gmail.com>
2020-09-18 11:24:50 -0400

    Script is running properly in console until for-loop. Need to force prompts for criteria

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Started to add values into total function
 Prompts currently working with testing, result is undefined
 Need to find way to not allow bypass of password length prompt
 Need to fix "undefined" once all criteria selected
+
+---
+
+Alternative fix to force password length - forces user to refresh page
+Same thing if no criteria selected --need to find way to keep prompting
+Code is now returning 1 character after generate password selected
+Need to find way to display entire code
+Can see entire code is working properly until the very end during for-loop where only 1 character is displayed
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var numbers = "0123456789";
 var symbols = "!@#$%&*()_-~?.";
 
 function generatePassword() {
-  var randomPass = "";
+  var password = "";
   var passFeatures = "";
 
   var passLength = parseInt(
@@ -15,70 +15,64 @@ function generatePassword() {
       "How long do you want your password to be? Please select a number between 8 - 128"
     )
   );
-
-  if (passLength < 8 || passLength > 128 || !passLength) {
-    alert("Please select a number between 8 - 128");
-    parseInt(
-      prompt(
-        "How long do you want your password to be? Please select a number between 8 - 128"
-      )
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert("Please refresh the page and select a number between 8 - 128");
+    return "";
+  } else {
+    var lowerCaseUse = confirm(
+      "would you like your password to include lower case characters?"
     );
-  } else if (passLength >= 8 && passLength <= 128) {
-  }
 
-  var lowerCaseUse = confirm(
-    "would you like your password to include lower case characters?"
-  );
-
-  if (lowerCaseUse) {
-    passFeatures = passFeatures + lowerCase;
-  }
-
-  var upperCaseUse = confirm(
-    "would you like your password to include upper case characters?"
-  );
-
-  if (upperCaseUse) {
-    passFeatures = passFeatures + upperCase;
-  }
-
-  var numberUse = confirm("Would you like your password to include numbers?");
-
-  if (numberUse) {
-    passFeatures = passFeatures + numberUse;
-  }
-
-  var symbolUse = confirm("Would you like your password to include symbols?");
-
-  if (symbolUse) {
-    passFeatures = passFeatures + symbolUse;
-  }
-
-  if (passLength) {
-    for (var i = 0; i < passLength; i++);
-    {
-      randomPass =
-        randomPass +
-        passFeatures.charAt(Math.floor(Math.random() * passFeatures.length));
+    if (lowerCaseUse) {
+      passFeatures = passFeatures + lowerCase;
     }
-  }
 
-  if (
-    lowerCaseUse === false &&
-    upperCaseUse === false &&
-    numberUse === false &&
-    symbolUse === false
-  ) {
-    alert("Please select at least one criteria to generate your password");
-    var lowerCaseUse;
+    var upperCaseUse = confirm(
+      "would you like your password to include upper case characters?"
+    );
+
+    if (upperCaseUse) {
+      passFeatures = passFeatures + upperCase;
+    }
+
+    var numberUse = confirm("Would you like your password to include numbers?");
+
+    if (numberUse) {
+      passFeatures = passFeatures + numbers;
+    }
+
+    var symbolUse = confirm("Would you like your password to include symbols?");
+
+    if (symbolUse) {
+      passFeatures = passFeatures + symbols;
+    }
+
+    if (
+      lowerCaseUse === false &&
+      upperCaseUse === false &&
+      numberUse === false &&
+      symbolUse === false
+    ) {
+      alert(
+        "Please refresh the page and select at least one criteria to generate your password"
+      );
+      return "";
+    } else {
+      for (var i = 0; i < passLength; i++) {
+        password =
+          password +
+          passFeatures.charAt(Math.floor(Math.random() * passFeatures.length));
+        return password;
+      }
+    }
   }
 }
 
 // Write password to the #password input
 function writePassword() {
+  var password = "";
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
